4c8bd0462e1902a8b00dde3f34910bdddc59cf13
januwA <ajanuw1995@gmail.com>
2020-10-19 15:46:14 +0800

    v0.3.0

--- test/dart_printf_test.dart
+++ test/dart_printf_test.dart
@@ -4,11 +4,11 @@ import 'package:test/test.dart';
 void main() {
   group('A group of tests', () {
     test('First Test', () {
-      expect(printf('%s %s XDD', 'hello', 'world'), 'hello world XDD');
-      expect(printf([1, 2, 3]), '[1, 2, 3]');
-      expect(printf(1), '1');
-      expect(printf(true), 'true');
-      expect(printf('%s %o', 'hi', [1,2], false), 'hi [1, 2]');
+      expect(printfr('%s %s XDD', 'hello', 'world'), 'hello world XDD');
+      expect(printfr([1, 2, 3]), '[1, 2, 3]');
+      expect(printfr(1), '1');
+      expect(printfr(true), 'true');
+      expect(printfr('%s %o', 'hi', [1,2], false), 'hi [1, 2]');
     });
   });
 }
